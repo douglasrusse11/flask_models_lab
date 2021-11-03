30cf6843444541e8dfc47a4d1b175a04b4e5337f
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-03 16:42:23 +0000

    restructures html, adds more css

--- models/order.py
+++ models/order.py
@@ -1,7 +1,9 @@
 class Order:
 
-    def __init__(self, customer_name, order_date, quantity, book_title):
+    def __init__(self, customer_name, order_date, quantity, book_title, description, url):
         self.customer_name = customer_name
         self.order_date = order_date
         self.quantity = quantity
         self.book_title = book_title
+        self.description = description
+        self.url = url
